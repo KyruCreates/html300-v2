b35d2142bd011d7a04f39eb85491d81f81be3d4d
KyruCreates <17752737+KyruCreates@users.noreply.github.com>
2021-04-22 19:52:55 -0700

    lesson3 activity

--- lesson03/activity/dogs.js
+++ lesson03/activity/dogs.js
@@ -1,5 +1,23 @@
 // Create the Dog class constructor
-
+function Dog(name, age, breed, color){
+  this.name = name;
+  this.age = age;
+  this.breed = breed;
+  this.color = color;
+  this.energyLevel = 0;
+  this.barkLevel = 10;
+  this.pat = function() {
+    this.energyLevel += 1;
+    this.barkLevel -= 1;
+    console.log(`This dog is called ${this.name} and is ${this.age} years old. Bark level is ${this.barkLevel}, energy level is ${this.energyLevel}.`)
+  }
+}
 // Create Marty
+const marty = new Dog("Marty", 8, "terrier", "brown");
 
 // Pat him 5 times
+marty.pat();
+marty.pat();
+marty.pat();
+marty.pat();
+marty.pat();
